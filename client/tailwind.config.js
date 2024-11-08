89fc974aea5d40d82e351272ea67e3ffb53e52b9
Morteza Khojasteh <mori.kh1987@gmail.com>
2024-11-08 13:03:38 +0000

    config tailwind CSS

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Scans all TypeScript and React files
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
